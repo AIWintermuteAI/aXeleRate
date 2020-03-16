60d7598bea438447f13f5906b992386d15b99b67
AIWintermuteAI <dmitrywat@gmail.com>
2020-03-16 18:18:19 +0800

    modified:   requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
+
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(name='axelerate',
       version="0.5.1",
       description='Keras-based framework for AI on the Edge',
+      install_requires=requirements,
       long_description=long_description,
       long_description_content_type="text/markdown",
       author='Dmitry Maslov',
